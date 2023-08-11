aa47e2010ae25762e1f5e1434abbcdd29b9759d7
EldorKarimov <hacktivist19021@gmail.com>
2023-08-11 14:24:35 +0500

    remove translate answer

--- main/translation.py
+++ main/translation.py
@@ -4,11 +4,3 @@ from .models import Leader, Appeal, Answer
 @register(Leader)
 class LeaderTranslationOptions(TranslationOptions):
     fields = ('position', )
-
-@register(Appeal)
-class AppealTranslationOptions(TranslationOptions):
-    fields = ('message', 'theme')
-
-@register(Answer)
-class AnswerTranslationOptions(TranslationOptions):
-    fields = ('message', )
